daf75b079df4d6c22298f5376cdce5d6a5e0db34
Francois Beutin <francois.beutin@ledger.fr>
2024-07-17 18:19:36 +0200

    Remove unused variable

--- src/handle_provide_token.c
+++ src/handle_provide_token.c
@@ -4,7 +4,5 @@
 // `msg->token1` and `msg->token2`. If those pointers are `NULL`, this means the ethereum app didn't
 // find any info regarding the requested tokens!
 void handle_provide_token(ethPluginProvideInfo_t *msg) {
-    context_t *context = (context_t *) msg->pluginContext;
-
     msg->result = ETH_PLUGIN_RESULT_OK;
 }
